7dfc670df9344fec956f71c085c392472513b67f
benetesla <78994881+benetesla@users.noreply.github.com>
2022-04-29 14:03:12 -0300

    Aulas de PHP
    #Conteudo das aulas de PHP

---
+++ Echo_print.php
@@ -0,0 +1,15 @@
+
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>PHP 7</title>
+</head>
+<body>
+<?php
+echo 'Comando Echo <br/>'; 
+print 'Comando Print<hr/>'
+?>
+</body>
+</html>
---
+++ var.php
@@ -0,0 +1,30 @@
+
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>PHP 7</title>
+</head>
+<body>
+	<?php
+//string
+$nome = 'Benevanio santos';
+//int
+$idade = 23;
+//float
+$peso = 62.89;
+//boolean
+$sonhos_sn = true //true or false
+
+	?>
+    <h1>Ficha cadastral</h1>
+    <br>
+    <p> Nome: <?= $nome ?> </p>
+     <p> Idade:	<?= $idade ?> </p>
+      <p> Peso:	<?= $peso ?> </p>
+        <p> Sonhos:	<?= $sonhos_sn ?> </p>
+
+</body>
+</html>
+
